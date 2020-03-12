c8caea7a3b8ac8ca45b010bce71543aa9e7e913e
Isaac Smith <isaacsmith215@aol.com>
2020-03-12 13:13:54 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,21 +1,25 @@
 def create_an_empty_array
-  
+  empty = []
 end
 
 def create_an_array
-  
+  array=["hi","hey","heyo","hello"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array = []
+  array.push(element)
 end
 
 def add_element_to_start_of_array(array, element)
+  array = []
+  array.unshift(element)
   
 end
 
 def remove_element_from_end_of_array(array)
-  
+  array = ["hey","hey"]
+  array.pop()
 end
 
 def remove_element_from_start_of_array(array)
